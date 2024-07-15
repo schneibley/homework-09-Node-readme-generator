e3d47e5e9a12671e5582a1097e959b4ffacc2c77
schneibley <Schneibley@gmail.com>
2024-07-15 15:27:06 -0400

    added 'tests' to the prompt and changed the inquirer to 'require'

--- readme-generator/index.js
+++ readme-generator/index.js
@@ -1,78 +1,77 @@
 const fs = require('fs');
+const inquirer = require('inquirer');  // Use require to import 'inquirer'
 
-//I couldn't get it to work when attempting to use 'require'
-import('inquirer')
-  .then((inquirer) => {
-    const { prompt } = inquirer.default || inquirer;
+function promptUser() {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'title',
+            message: 'Enter the project title:'
+        },
+        {
+            type: 'input',
+            name: 'description',
+            message: 'Enter a project description:'
+        },
+        {
+            type: 'input',
+            name: 'installation',
+            message: 'Enter installation instructions:'
+        },
+        {
+            type: 'input',
+            name: 'usage',
+            message: 'Enter usage information:'
+        },
+        {
+            type: 'input',
+            name: 'contributing',
+            message: 'Enter contribution guidelines:'
+        },
+        {
+            type: 'input',
+            name: 'tests',
+            message: 'Enter test instructions:'
+        },
+        {
+            type: 'list',
+            name: 'license',
+            message: 'Choose a license for your application:',
+            choices: ['MIT', 'Apache 2.0', 'GPL 3.0']
+        },
+        {
+            type: 'input',
+            name: 'githubUsername',
+            message: 'Enter your GitHub username:'
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'Enter your email address:'
+        }
+    ]);
+}
 
-
-    function promptUser() {
-        return prompt([
-            {
-                type: 'input',
-                name: 'title',
-                message: 'Enter the project title:'
-            },
-            {
-                type: 'input',
-                name: 'description',
-                message: 'Enter a project description:'
-            },
-            {
-                type: 'input',
-                name: 'installation',
-                message: 'Enter installation instructions:'
-            },
-            {
-                type: 'input',
-                name: 'usage',
-                message: 'Enter usage information:'
-            },
-            {
-                type: 'input',
-                name: 'contributing',
-                message: 'Enter contribution guidelines:'
-            },
-            {
-                type: 'list',
-                name: 'license',
-                message: 'Choose a license for your application:',
-                choices: ['MIT', 'Apache 2.0', 'GPL 3.0']
-            },
-            {
-                type: 'input',
-                name: 'githubUsername',
-                message: 'Enter your GitHub username:'
-            },
-            {
-                type: 'input',
-                name: 'email',
-                message: 'Enter your email address:'
-            }
-        ]);
+function generateMarkdown(data) {
+    let licenseBadge = '';
+    if (data.license === 'MIT') {
+        licenseBadge = '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)';
+    } else if (data.license === 'Apache 2.0') {
+        licenseBadge = '[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)';
+    } else if (data.license === 'GPL 3.0') {
+        licenseBadge = '[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)';
     }
 
-    function generateMarkdown(data) {
+    let licenseInfo = '';
+    if (data.license === 'MIT') {
+        licenseInfo = 'This application is covered under the [MIT license](https://opensource.org/licenses/MIT).';
+    } else if (data.license === 'Apache 2.0') {
+        licenseInfo = 'This application is covered under the [Apache 2.0 license](https://opensource.org/licenses/Apache-2.0).';
+    } else if (data.license === 'GPL 3.0') {
+        licenseInfo = 'This application is covered under the [GPL v3 license](https://www.gnu.org/licenses/gpl-3.0).';
+    }
 
-        let licenseBadge = '';
-        if (data.license === 'MIT') {
-            licenseBadge = '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)';
-        } else if (data.license === 'Apache 2.0') {
-            licenseBadge = '[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)';
-        } else if (data.license === 'GPL 3.0') {
-            licenseBadge = '[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)';
-        }
-
-        let licenseInfo = '';
-        if (data.license === 'MIT') {
-            licenseInfo = 'This application is covered under the [MIT license](https://opensource.org/licenses/MIT).';
-        } else if (data.license === 'Apache 2.0') {
-            licenseInfo = 'This application is covered under the [Apache 2.0 license](https://opensource.org/licenses/Apache-2.0).';
-        } else if (data.license === 'GPL 3.0') {
-            licenseInfo = 'This application is covered under the [GPL v3 license](https://www.gnu.org/licenses/gpl-3.0).';
-        }
-
-        const markdownContent = `
+    const markdownContent = `
 # ${data.title}
 
 ## Description
@@ -83,6 +82,7 @@ ${data.description}
 - [Usage](#usage)
 - [License](#license)
 - [Contributing](#contributing)
+- [Tests](#tests)
 - [Questions](#questions)
 
 ## Installation
@@ -99,38 +99,36 @@ ${licenseInfo}
 ## Contributing
 ${data.contributing}
 
+## Tests
+${data.tests}
+
 ## Questions
 For questions about this project, please contact:
 - GitHub: [${data.githubUsername}](https://github.com/${data.githubUsername})
 - Email: ${data.email}
 `;
 
-        return markdownContent;
+    return markdownContent;
+}
+
+function writeToFile(fileName, data) {
+    fs.writeFileSync(fileName, data);
+}
+
+async function init() {
+    try {
+        console.log('Welcome to the README.md generator!\n');
+
+        const userResponses = await promptUser();
+
+        const markdown = generateMarkdown(userResponses);
+
+        writeToFile('README.md', markdown);
+
+        console.log('Successfully created README.md file!');
+    } catch (error) {
+        console.error('Error generating README:', error);
     }
+}
 
-    function writeToFile(fileName, data) {
-        fs.writeFileSync(fileName, data);
-    }
-
-    async function init() {
-        try {
-            console.log('Welcome to the README.md generator!\n');
-
-            const userResponses = await promptUser();
-
-            const markdown = generateMarkdown(userResponses);
-
-            writeToFile('README.md', markdown);
-
-            console.log('Successfully created README.md file!');
-        } catch (error) {
-            console.error('Error generating README:', error);
-        }
-    }
-
-    
-    init();
-  })
-  .catch(err => {
-    console.error('Failed to import inquirer:', err);
-  });
+init();
